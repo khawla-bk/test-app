5dfb55c80fc5d88e80da4cc2eead8d88e3740534
khawla-bk <benkadidakhawla@gmail.com>
2023-05-19 14:05:58 +0100

    added basic css, components, and containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./App.css";
+import {
+  About,
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <About />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
